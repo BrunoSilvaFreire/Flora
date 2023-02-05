643ff42141e8cab85d5ac0a04426bd596123a25f
BrunoSilvaFreire <brunorbsf@gmail.com>
2023-02-05 12:28:15 -0300

    Pulla

--- Assets/Flora/Scripts/Player/Pulla.cs
+++ Assets/Flora/Scripts/Player/Pulla.cs
@@ -10,10 +10,15 @@ namespace Flora.Scripts.Player {
         public CharacterController controller;
         public float jumpHeight;
         public float moveSpeed;
-        private bool _grounded;
+        public float gravityScale;
+        public float gravityScaleWhenHoldingJump;
+        public float maxSpeed;
+        public float decelerationThreshold;
+        public float decelerationSpeed;
 
         private bool _attached;
         private InputAction _move, _jump;
+        private CollisionFlags _collisionFlags;
 
         public void Attach(PlayerInput input) {
             _attached = true;
@@ -39,18 +44,31 @@ namespace Flora.Scripts.Player {
         }
 
         private void FixedUpdate() {
-            var velocity = new Vector3(move.x, 0, move.y);
-            velocity.Normalize();
-            velocity *= moveSpeed;
+            var velocity = controller.velocity;
 
-            var shouldJump = jump.Consume() && _grounded;
-            velocity += Physics.gravity * Time.fixedDeltaTime;
+            var inputDirection = new Vector3(move.x, 0, move.y).normalized;
+            if (inputDirection.magnitude > decelerationThreshold) {
+                velocity += inputDirection * moveSpeed;
+            } else {
+                velocity.x = Mathf.Lerp(velocity.x, 0, decelerationSpeed);
+                velocity.z = Mathf.Lerp(velocity.z, 0, decelerationSpeed);
+            }
+
+            var grounded = (_collisionFlags & CollisionFlags.Below) != 0;
+            var gravityScale = jump.Current ? gravityScaleWhenHoldingJump : this.gravityScale;
+            var shouldJump = jump.Consume() && grounded;
+
+            velocity += Physics.gravity * gravityScale;
 
             if (shouldJump) {
                 velocity.y = jumpHeight;
             }
-
-            _grounded = controller.SimpleMove(velocity * moveSpeed);
+            var horizontal = new Vector2(velocity.x, velocity.z);
+            horizontal = Vector2.ClampMagnitude(horizontal, maxSpeed);
+            velocity.x = horizontal.x;
+            velocity.z = horizontal.y;
+            
+            _collisionFlags = controller.Move(velocity * Time.fixedDeltaTime);
         }
     }
 }
